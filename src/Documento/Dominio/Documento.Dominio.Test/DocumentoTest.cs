4a3c5eb472acfb69afdcd39ebe80d15548ea74a1
Ruben Pabon <benru_203@hotmail.com>
2025-10-14 14:46:03 -0500

    test (Documento): se espera que se cree el documento

---
+++ src/Documento/Dominio/Documento.Dominio.Test/DocumentoTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Documento.Dominio.Test
+{
+    public class DocumentoTest
+    {
+
+
+        [Fact]
+        public void CrearDocumento_DeberiaCrearCorrectamente()
+        {
+
+            var titulo = "Contrato 123";
+            var autor = "Ruben Pabon";
+            var tipo = "Contrato";
+            var estado = "Pendiente";
+
+            var documento = new Documento(titulo, autor, tipo, estado);
+
+            Assert.NotNull(documento);
+            Assert.NotEqual(Guid.Empty, documento.Id);
+            Assert.Equal(titulo, documento.Titulo);
+            Assert.Equal(autor, documento.Autor);
+            Assert.Equal(tipo, documento.Tipo);
+            Assert.Equal(estado, documento.Estado);
+            Assert.True(documento.FechaRegistro <= DateTime.UtcNow);
+        }
+    }
+}
