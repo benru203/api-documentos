fbe1b411ec31e678fd433c99d706847fcb869909
Ruben Pabon <benru_203@hotmail.com>
2025-10-14 16:18:35 -0500

    test: (Documento): se espera que falle si viene con titulo, autor, tipo invalido o en tipo no este en el listado tipos

--- src/Documento/Dominio/Documento.Dominio.Test/DocumentoTest.cs
+++ src/Documento/Dominio/Documento.Dominio.Test/DocumentoTest.cs
@@ -10,18 +10,61 @@
 
             var titulo = "Contrato 123";
             var autor = "Ruben Pabon";
-            var tipo = "Contrato";
-            var estado = "Pendiente";
+            var tipo = "CONTRATO";
+            var estado = "PENDIENTE";
 
             var documento = new Entidades.Documento(titulo, autor, tipo, estado);
 
             Assert.NotNull(documento);
             Assert.NotEqual(Guid.Empty, documento.Id);
-            Assert.Equal(titulo, documento.Titulo);
-            Assert.Equal(autor, documento.Autor);
-            Assert.Equal(tipo, documento.Tipo);
+            Assert.Equal(titulo, documento.Titulo.Valor);
+            Assert.Equal(autor, documento.Autor.Valor);
+            Assert.Equal(tipo, documento.Tipo.Valor);
             Assert.Equal(estado, documento.Estado);
             Assert.True(documento.FechaRegistro <= DateTime.UtcNow);
         }
+
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void CrearDocumento_ConTituloInvalido_DebeLanzarExcepcion(string tituloInvalido)
+        {
+            var excepcion = Assert.Throws<ArgumentException>(() =>
+                new Entidades.Documento(tituloInvalido, "Autor", "CONTRATO", "PENDIENTE")
+            );
+
+            Assert.Contains("título", excepcion.Message, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void CrearDocumento_ConAutorInvalido_DebeLanzarExcepcion(string autorInvalido)
+        {
+            var excepcion = Assert.Throws<ArgumentException>(() =>
+               new Entidades.Documento("Contrato 123", autorInvalido, "CONTRATO", "PENDIENTE")
+           );
+
+            Assert.Contains("autor", excepcion.Message, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("REPORTE")]
+        [InlineData(null)]
+        public void CrearDocumento_ConTipoInvalido_DebeLanzarExcepcion(string tipoInvalido)
+        {
+            var excepcion = Assert.Throws<ArgumentException>(() =>
+               new Entidades.Documento("Contrato 123", "RubenPabon", tipoInvalido, "PENDIENTE")
+           );
+
+            Assert.Contains("tipo", excepcion.Message, StringComparison.OrdinalIgnoreCase);
+        }
     }
+
 }
