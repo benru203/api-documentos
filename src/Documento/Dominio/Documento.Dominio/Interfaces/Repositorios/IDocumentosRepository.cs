8e9e875729de2e6a86343c9f29718c8cfe434fc4
Ruben Pabon <benru_203@hotmail.com>
2025-10-15 05:21:29 -0500

    feat (Documento) : se crea la interfaces IDocumentoRepository

---
+++ src/Documento/Dominio/Documento.Dominio/Interfaces/Repositorios/IDocumentosRepository.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Documento.Dominio.Interfaces.Repositorios
+{
+    public interface IDocumentosRepository
+    {
+        Task<IEnumerable<Entidades.Documento>> GetAllAsync();
+
+        Task<Entidades.Documento> GetByIdAsync(Guid id);
+
+        Task AddAsync(Entidades.Documento documento);
+
+        Task UpdateAsync(Entidades.Documento documento);
+
+        Task DeleteAsync(Guid id);
+    }
+}
