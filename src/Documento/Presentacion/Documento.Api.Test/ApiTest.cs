c190b519413eae279bbaac8a55bf38fefe96720c
Ruben Pabon <benru_203@hotmail.com>
2025-10-15 16:28:43 -0500

    test(api): se espera un status 201 y el id del nuevo documento

--- src/Documento/Presentacion/Documento.Api.Test/ApiTest.cs
+++ src/Documento/Presentacion/Documento.Api.Test/ApiTest.cs
@@ -1,5 +1,7 @@
 using Documento.Api.Controllers;
+using Documento.Aplicacion.DTOs;
 using Documento.Aplicacion.Servicios;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace Documento.Api.Test
@@ -17,6 +19,32 @@ namespace Documento.Api.Test
             );
 
             _controller = new DocumentoController(_documentoServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task CrearDocumento_DebeRetornar_201_Body_NuevoId_Correctamente()
+        {
+            var nuevoDocumento = new CrearDocumentoDTO
+            {
+                Titulo = "Contrato 123",
+                Autor = "Ruben Pabon",
+                Tipo = "CONTRATO",
+                Estado = "PENDIENTE"
+            };
+
+            var documentoId = Guid.NewGuid();
+
+            _documentoServiceMock.Setup(s => s.CreaDocumento(nuevoDocumento)).ReturnsAsync(documentoId);
+
+            var result = await _controller.crearDocumento(nuevoDocumento);
+
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+            Assert.Equal(201, createdResult.StatusCode);
+
+            var body = Assert.IsType<Dictionary<string, object>>(createdResult.Value);
+            Assert.Equal(documentoId, body["id"]);
+
+            _documentoServiceMock.Verify(s => s.CreaDocumento(It.IsAny<CrearDocumentoDTO>()), Times.Once);
         }
 
     }
