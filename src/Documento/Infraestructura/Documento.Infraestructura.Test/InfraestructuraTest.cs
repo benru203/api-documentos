e7ff03db9794abf03ac482e0e3012d1be3397570
Ruben Pabon <benru_203@hotmail.com>
2025-10-15 10:35:28 -0500

    test(infraestructura): se espera que se almacene en la base de datos un nuevo documento

--- src/Documento/Infraestructura/Documento.Infraestructura.Test/InfraestructuraTest.cs
+++ src/Documento/Infraestructura/Documento.Infraestructura.Test/InfraestructuraTest.cs
@@ -1,7 +1,53 @@
+using Documento.Infraestructura.Context;
+using Documento.Infraestructura.Repositorios;
+using Microsoft.EntityFrameworkCore;
+
 namespace Documento.Infraestructura.Test
 {
     public class InfraestructuraTest
     {
+        private readonly DocumentoContext _context;
+
+        private readonly DocumentoRepository _documentoRepository;
+
+        public InfraestructuraTest()
+        {
+            var options = new DbContextOptionsBuilder<DocumentoContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+            _context = new DocumentoContext(options);
+            _documentoRepository = new DocumentoRepository(_context);
+
+            //seed de datos
+            List<Dominio.Entidades.Documento> documentos = new List<Dominio.Entidades.Documento> {
+                new("Contrato 123", "Ruben Pabon", "CONTRATO", "PENDIENTE"),
+                new("Informe 456", "Ruben Pabon", "INFORME", "REGISTRADO"),
+                new("Acta 789", "Ruben Pabon", "ACTA", "PENDIENTE")
+            };
+
+            _context.Documentos.AddRangeAsync(documentos);
+            _context.SaveChanges();
+
+        }
+
+        [Fact]
+        public async Task CreaDocumento_DebeAlmacenarloDB()
+        {
+            var titulo = "Contrato 741";
+            var autor = "John Doe";
+            var tipo = "CONTRATO";
+            var estado = "ARCHIVADO";
+            var documento = new Dominio.Entidades.Documento(titulo, autor, tipo, estado);
+
+            await _documentoRepository.AddAsync(documento);
+
+            var documentoGuardado = await _context.Documentos.FirstOrDefaultAsync(d => d.Id == documento.Id);
+
+            Assert.NotNull(documentoGuardado);
+            Assert.Equal(documento.Id, documentoGuardado.Id);
+            Assert.Equal(documento.FechaRegistro, documentoGuardado.FechaRegistro);
+        }
+
 
     }
 }
