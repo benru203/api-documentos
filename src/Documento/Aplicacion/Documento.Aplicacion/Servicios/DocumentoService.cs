2c14ce711413919dc47b303451f0b74941cdebc4
Ruben Pabon <benru_203@hotmail.com>
2025-10-15 05:23:26 -0500

    feat (aplicacion): se implementa el test para creacion de documento

---
+++ src/Documento/Aplicacion/Documento.Aplicacion/Servicios/DocumentoService.cs
@@ -0,0 +1,25 @@
+﻿using Documento.Aplicacion.DTOs;
+using Documento.Dominio.Interfaces.Repositorios;
+
+namespace Documento.Aplicacion.Servicios
+{
+    public class DocumentoService
+    {
+
+        private readonly IDocumentosRepository _documentosRepository;
+
+
+        public DocumentoService(IDocumentosRepository documentosRepository)
+        {
+            _documentosRepository = documentosRepository;
+        }
+
+
+        public async Task<Guid> CreaDocumento(CrearDocumentoDTO crearDocumentoDTO)
+        {
+            var documento = new Dominio.Entidades.Documento(crearDocumentoDTO.Titulo, crearDocumentoDTO.Autor, crearDocumentoDTO.Tipo, crearDocumentoDTO.Estado);
+            await _documentosRepository.AddAsync(documento);
+            return documento.Id;
+        }
+    }
+}
