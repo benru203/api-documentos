203d8697a9c729129d8014ef6f0a5c2348ec40a9
Ruben Pabon <benru_203@hotmail.com>
2025-10-15 11:03:46 -0500

    feat(DocumentoRepository): se implementa la busqueda por id del documento

---
+++ src/Documento/Infraestructura/Documento.Infraestructura/Repositorios/DocumentoRepository.cs
@@ -0,0 +1,42 @@
+﻿using Documento.Dominio.Interfaces.Repositorios;
+using Documento.Infraestructura.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Documento.Infraestructura.Repositorios
+{
+    public class DocumentoRepository : IDocumentoRepository
+    {
+
+        private readonly DocumentoContext _context;
+
+        public DocumentoRepository(DocumentoContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Dominio.Entidades.Documento documento)
+        {
+            await _context.AddAsync(documento);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Dominio.Entidades.Documento>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Dominio.Entidades.Documento> GetByIdAsync(Guid id)
+        {
+            return await _context.Documentos.FirstOrDefaultAsync(d => d.Id == id);
+        }
+
+        public Task UpdateAsync(Dominio.Entidades.Documento documento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
