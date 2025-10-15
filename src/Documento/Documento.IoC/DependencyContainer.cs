5c2218e6c3713d6ac572b27fc7fc5391509446f9
Ruben Pabon <benru_203@hotmail.com>
2025-10-15 15:55:38 -0500

    chore(IoC) se crea contenedor de dependencia de documento

---
+++ src/Documento/Documento.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Documento.Infraestructura;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Documento.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddDocumentoService(this IServiceCollection services, string stringConexion)
+        {
+            services.AddDocumentoInfraestructura(stringConexion);
+            return services;
+        }
+    }
+}
