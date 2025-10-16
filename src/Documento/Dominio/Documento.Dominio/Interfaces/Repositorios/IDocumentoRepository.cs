b7a58d2367ac7ab9d0cdca7b1edd340a5fa6ac00
Ruben Pabon <benru_203@hotmail.com>
2025-10-16 08:34:06 -0500

    refactor(IDocumementoRepository): se modifica el getAll para enviar meta data de paginacion

--- src/Documento/Dominio/Documento.Dominio/Interfaces/Repositorios/IDocumentoRepository.cs
+++ src/Documento/Dominio/Documento.Dominio/Interfaces/Repositorios/IDocumentoRepository.cs
@@ -3,7 +3,7 @@ namespace Documento.Dominio.Interfaces.Repositorios
 {
     public interface IDocumentoRepository
     {
-        Task<IEnumerable<Entidades.Documento>> GetAllAsync(int pagina, int tamanoPagina);
+        Task<(IEnumerable<Entidades.Documento>, int)> GetAllAsync(int pagina, int tamanoPagina);
 
         Task<Entidades.Documento> GetByIdAsync(Guid id);
 
